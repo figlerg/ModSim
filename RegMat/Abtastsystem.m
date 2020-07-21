bb828d167ba7f1a7c9b3074555cc32b3cd754329
FiglerG <Figlergelix@gmail.com>
2020-07-21 12:54:47 +0200

    Worksheet 1 pretty much finished, only errors missing.

--- RegMat/Abtastsystem.m
+++ RegMat/Abtastsystem.m
@@ -12,8 +12,8 @@ x0 = [0.1; 0.1];
 sys = ss(A,B,C,D); % construct system model automatically
 
 figure(1);
-[y_lsim,~,~] = lsim(sys, u1, t, x0) % simulation of response
-plot(t,y_lsim);
+[y_lsim_sin,~,~] = lsim(sys, u1, t, x0) % simulation of response
+plot(t,y_lsim_sin);
 title('Response y for u = sin(x), x(0) = (' + strjoin(string(x0),',')+')')
 
 % case 2
@@ -25,23 +25,23 @@ title('Response y to u = 0, x(0) = (' + strjoin(string(X_0_2),',')+')')
 
 % Task 2
 % [t,y] = forward_euler(h, time, X_0,U);
-[t,y] = forward_euler(h, time, x0,u1);
+[t,y_euler_sin] = forward_euler(h, time, x0,u1);
 figure(3);
-y_lsim = y_lsim'
-plot(t,y,t,y_lsim);
+y_lsim_sin = y_lsim_sin'
+plot(t,y_euler_sin,t,y_lsim_sin);
 title('Explicit euler vs. lsim')
 
 % implicit euler- how to get x_k?
 [t,y2] = backward_euler(h, time, x0,u1);
 figure(4);
-plot(t,y2,t,y_lsim);
+plot(t,y2,t,y_lsim_sin);
 title('Implicit euler vs. lsim')
 
 
 figure(5)
 sysd = c2d(sys, h);
 [Ysind,~,~] = lsim(sysd, u1, t, x0);
-plot(t,Ysind,t,y_lsim,t,y, t, y2);
+plot(t,Ysind,t,y_lsim_sin,t,y_euler_sin, t, y2);
 title('explicit euler vs. c2d vs. lsim vs. implicit euler');
 legend('c2d','lsim','explicit euler', 'implicit euler');
 
